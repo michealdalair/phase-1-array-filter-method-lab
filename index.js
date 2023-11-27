8ca7c445bac480e082d4ca7a1e112c36b0621e22
michaljones <micheal.jones2010@yahoo.com>
2023-11-27 15:54:46 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+
+
+function findMatching(names,string){
+    return names.filter(name=>{
+        return name.toLowerCase()=== string.toLowerCase()
+    })
+
+}
+
+
+function  fuzzyMatch(names,string){
+    return names.filter(name=>{
+        return name.startsWith(string)
+    })
+
+
+    
+}
+
+
+function  matchName(drivers,string){
+    return drivers.filter(obj =>{
+    return obj.name===string;
+    })
+
+}
